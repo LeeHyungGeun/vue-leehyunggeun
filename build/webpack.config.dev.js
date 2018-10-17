b844ff7d7cf6ca28b8bc1f0caea049a521246a2b
leehyunggeun <leehyunggeun@navercorp.com>
2018-10-18 00:28:15 +1100

    add minimize css module as one file

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 function resolve (dir) {
@@ -43,6 +44,7 @@ module.exports = {
       {
         test: /\.styl(us)?$/,
         use: [
+          MiniCssExtractPlugin.loader,
           'vue-style-loader',
           'css-loader',
           'stylus-loader'
@@ -64,6 +66,9 @@ module.exports = {
         to: resolve('dist/static/img'),
         toType: 'dir'
       }
-    ])
+    ]),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    })
   ]
 }
