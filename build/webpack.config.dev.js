5070d232ac095750aa646177bbfd3fccb7b2223f
leehyunggeun <leehyunggeun@navercorp.com>
2018-10-17 23:32:50 +1100

    add webpack config

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
