b29d602109a16d711577ac839411481440e2fdb7
leehyunggeun <leehyunggeun@navercorp.com>
2018-10-20 00:24:21 +1100

    add vue-router to route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '../assets/app.styl'
+import Vue from 'vue/dist/vue.js'
+import VueRouter from 'vue-router'
 
-/* eslint-disable-next-line no-new */
-new Vue({
-  el: '#app',
-  render: h => h(App)
+Vue.use(VueRouter)
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+const NotFoundView = { template: '<div>NotFoundView</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo, name: 'Foo' },
+  { path: '/bar', component: Bar, name: 'Bar' },
+  { path: '*', component: NotFoundView }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
 })
+
+new Vue({
+  router
+}).$mount('#app')
