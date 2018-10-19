6f9642dc72160669e1a4d06ac5847751f7bada4a
leehyunggeun <leehyunggeun@navercorp.com>
2018-10-19 22:42:07 +1100

    remove MiniCssExtract

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -4,7 +4,6 @@ const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 function resolve (dir) {
@@ -44,7 +43,6 @@ module.exports = {
       {
         test: /\.styl(us)?$/,
         use: [
-          MiniCssExtractPlugin.loader,
           'vue-style-loader',
           'css-loader',
           'stylus-loader'
@@ -66,9 +64,6 @@ module.exports = {
         to: resolve('dist/static/img'),
         toType: 'dir'
       }
-    ]),
-    new MiniCssExtractPlugin({
-      filename: 'main.css'
-    })
+    ])
   ]
 }
